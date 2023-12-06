335fef4f65df7e72ab831559e549e92c5b3b2373
Tiburso <manuelmariamc@gmail.com>
2023-12-06 17:00:12 +0100

    Add Company/Application and validation

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,54 @@
 package main
 
-func CreateApplication() {
+import (
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/Tiburso/GoManager/pkg/application"
+)
+
+type Company *application.Company
+type Application *application.Application
+
+func CreateCompany(companies map[string]Company, name string) error {
+	var candidatePortal string
+
+	fmt.Print("Enter company candidate portal: ")
+	fmt.Scanln(&candidatePortal)
+
+	company, err := application.NewCompany(name, candidatePortal)
+
+	if err != nil {
+		return err
+	}
+
+	companies[name] = company
+
+	return nil
+}
+
+func CreateApplication(companies map[string]Company, applications map[string]Application) error {
+
+	var name, applicationType, applicationDate, companyName string
+
+	fmt.Print("Enter application name: ")
+	fmt.Scanln(&name)
+	fmt.Print("Enter application type: ")
+	fmt.Scanln(&applicationType)
+	fmt.Print("Enter application date: ")
+	fmt.Scanln(&applicationDate)
+	fmt.Print("Enter company name: ")
+	fmt.Scanln(&companyName)
+
+	_, ok := companies[companyName]
+
+	if !ok {
+		fmt.Println("Company not found!")
+		CreateCompany(companies, companyName)
+	}
+
+	return nil
 }
 
 func DeleteApplication() {
@@ -12,7 +59,54 @@ func UpdateApplication() {
 
 }
 
-func main() {
-	// read user input from stdin
+func ShowApplications() {
 
+}
+
+func ShowCompanies() {
+
+}
+
+func ShowMenu() {
+	fmt.Println("1. Create application")
+	fmt.Println("2. Delete application")
+	fmt.Println("3. Update application")
+	fmt.Println("4. Show applications")
+	fmt.Println("5. Show companies")
+	fmt.Println("6. Exit")
+	fmt.Print("Enter your choice: ")
+}
+
+func main() {
+	companies := make(map[string]Company)
+	applications := make(map[string]Application)
+
+	for {
+		// show menu
+		ShowMenu()
+
+		// read user input from stdin
+		var input string
+		fmt.Scanln(&input)
+
+		input = strings.TrimSpace(input)
+
+		// switch on user input
+		switch input {
+		case "1":
+			CreateApplication(companies, applications)
+		case "2":
+			DeleteApplication()
+		case "3":
+			UpdateApplication()
+		case "4":
+			ShowApplications()
+		case "5":
+			ShowCompanies()
+		case "6":
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid input")
+		}
+	}
 }
--- pkg/application/application.go
+++ pkg/application/application.go
@@ -1,15 +1,84 @@
 package application
 
-import "time"
+import (
+	"fmt"
+	"regexp"
+	"time"
+)
 
 type Company struct {
 	Name            string
 	CandidatePortal string
 }
 
+const (
+	FullTime   = "Full Time"
+	PartTime   = "Part Time"
+	Internship = "Internship"
+)
+
 type Application struct {
 	Name            string
 	Type            string
 	ApplicationDate time.Time
 	Company         Company
+}
+
+func isValidURL(url string) bool {
+	// Regular expression for a basic URL validation
+	// Note: This is a simple example and might not cover all edge cases.
+	urlPattern := `^(http|https)://[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]+$`
+
+	match, err := regexp.MatchString(urlPattern, url)
+	if err != nil {
+		// Handle error if the regular expression compilation fails
+		fmt.Println("Error:", err)
+		return false
+	}
+
+	return match
+}
+
+// COMPANY
+
+func NewCompany(name, candidatePortal string) (*Company, error) {
+	// Candidate portal must be a valid URL
+	if !isValidURL(candidatePortal) {
+		return nil, fmt.Errorf("'%s' is not a valid URL", candidatePortal)
+	}
+
+	return &Company{
+		Name:            name,
+		CandidatePortal: candidatePortal,
+	}, nil
+}
+
+func (c Company) String() string {
+	return c.Name + ", " + c.CandidatePortal
+}
+
+// APPLICATION
+
+func NewApplication(name, applicationType, applicationDate string, company Company) (*Application, error) {
+	// Application type must be one of the following: Full Time, Part Time, Internship
+	if applicationType != FullTime && applicationType != PartTime && applicationType != Internship {
+		return nil, fmt.Errorf("'%s' is not a valid application type", applicationType)
+	}
+
+	// Application date must be a valid date
+	date, err := time.Parse("2006-01-02", applicationDate)
+	if err != nil {
+		return nil, fmt.Errorf("'%s' is not a valid date", applicationDate)
+	}
+
+	return &Application{
+		Name:            name,
+		Type:            applicationType,
+		ApplicationDate: date,
+		Company:         company,
+	}, nil
+}
+
+func (a Application) String() string {
+	return a.Name + ", " + a.Type + ", " + a.ApplicationDate.String() + ", " + a.Company.Name + ", " + a.Company.CandidatePortal
 }

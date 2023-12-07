a2c489cf1b48eabba7d6a673fd67c65f5ecfdf7b
Tiburso <manuelmariamc@gmail.com>
2023-12-07 23:51:13 +0100

    Refactor application update and delete handlers

--- internal/handler/controllers/application.go
+++ internal/handler/controllers/application.go
@@ -62,18 +62,19 @@ func DeleteApplicationHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Initialize the application variable
-	app := &application.Application{}
+	vars := r.URL.Query()
 
-	// Decode JSON from the request body
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(app); err != nil {
+	name, name_ok := vars["name"]
+	company_name, company_name_ok := vars["company_name"]
+
+	if !name_ok || !company_name_ok {
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(err.Error())
+		json.NewEncoder(w).Encode("Missing name or company_name")
 		return
 	}
 
 	// Delete the application from the database
-	res := database.DB.Where("name = ? AND company_name = ?", app.Name, app.CompanyName).Delete(&application.Application{})
+	res := database.DB.Where("name = ? AND company_name = ?", name, company_name).Delete(&application.Application{})
 
 	if res.Error != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -83,25 +84,76 @@ func DeleteApplicationHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Send a JSON response
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(app)
 }
 
 func UpdateApplicationHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Initialize the application variable
-	app := &application.Application{}
+	update_map := make(map[string]any)
 
 	// Decode JSON from the request body
 	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(app); err != nil {
+	if err := decoder.Decode(&update_map); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
 
+	name, name_ok := update_map["name"]
+	company_name, company_name_ok := update_map["company_name"]
+
+	if !name_ok || !company_name_ok {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("Missing name or company_name")
+		return
+	}
+
 	// Update the application in the database
-	res := database.DB.Where("name = ? AND company_name = ?", app.Name, app.CompanyName).Save(&app)
+	var app application.Application
+	res := database.DB.First("name = ? AND company_name = ?", name, company_name)
+
+	if res.Error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(res.Error.Error())
+		return
+	}
+
+	// Now we have the application, we can update it
+	status, status_ok := update_map["status"]
+
+	if status_ok {
+		err := app.SetStatus(status.(string))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode("Invalid status")
+			return
+		}
+	}
+
+	application_date, application_date_ok := update_map["application_date"]
+
+	if application_date_ok {
+		err := app.SetApplicationDate(application_date.(string))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode("Invalid application date")
+			return
+		}
+	}
+
+	application_type, application_type_ok := update_map["application_type"]
+
+	if application_type_ok {
+		err := app.SetType(application_type.(string))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode("Invalid application type")
+			return
+		}
+	}
+
+	res = database.DB.Save(&app)
 
 	if res.Error != nil {
 		w.WriteHeader(http.StatusInternalServerError)

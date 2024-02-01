38240e69059047225f921b0fad9de4f7d3e21210
Tiburso <manuelmariamc@gmail.com>
2024-02-01 17:58:37 +0100

    Add bottom up feature to edit and delete buttons

--- client/ui/companies/CompanyCard.tsx
+++ client/ui/companies/CompanyCard.tsx
@@ -10,7 +10,6 @@ import Link from 'next/link';
 import style from '@/styles//company/CompanyCard.module.css';
 
 export default function CompanyCard({ company }: { company: Company }) {
-  const [showButtons, setShowButtons] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   return (
@@ -27,7 +26,7 @@ export default function CompanyCard({ company }: { company: Company }) {
       {/* here i will have two buttons that will show up as i hover over the card */}
       {
         <div className={style.btns}>
-          <Button onClick={() => console.log('delete')}>Delete</Button>
+          <Button className={style.delete} onClick={() => console.log('delete')}>Delete</Button>
 
           {/* this button should be a form hidden inputs */}
           <Button onClick={() => setShowModal(true)} type="submit">
